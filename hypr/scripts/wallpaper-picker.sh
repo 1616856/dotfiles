bef1fe50ca89ca4579e3d65b8755bfe97a65d755
Omar <oshalaby75@gmail.com>
2026-04-27 13:17:28 -0400

    FIXED: Waybar finally autothemes using local color pipe

--- hypr/scripts/wallpaper-picker.sh
+++ hypr/scripts/wallpaper-picker.sh
@@ -6,14 +6,25 @@ for i in $(ls "$DIR"); do
     LIST+="$i\0icon\x1f$DIR/$i\n"
 done
 
-CHOICE=$(echo -e "$LIST" | rofi -dmenu -i -p "Gallery" -theme "$HOME/dotfiles/rofi/wallpaper-grid.rasi" -show-icons)
+CHOICE=$(echo -e "$LIST" | rofi -dmenu -i -p "Gallery" -theme "$HOME/dotfiles/rofi/pywal.rasi" -show-icons)
 
 if [ -n "$CHOICE" ]; then
     FULL_PATH="$DIR/$CHOICE"
-    awww img "$FULL_PATH"
+    
+    # 1. Apply wallpaper
+    awww img "$FULL_PATH" --transition-type grow --transition-pos top-right --transition-duration 2
+    
+    # 2. Generate colors
     wal -i "$FULL_PATH"
+    
+    # 3. FORCE colors into dotfiles (This is what you wanted)
+    cp "$HOME/.cache/wal/colors-waybar.css" "$HOME/dotfiles/waybar/colors.css"
+    
+    # 4. Nuclear Restart
+    pkill waybar
+    sleep 0.3
+    hyprctl dispatch exec waybar
     hyprctl reload
-    killall -SIGUSR2 waybar
 else
     exit 0
 fi
